8cda2b0cfe94f4bcfe98551c41175e3bc6c6e04b
Smith Saini <smithsaini1947@gmail.com>
2024-02-02 18:50:29 +0530

    First Commit

---
+++ play.js
@@ -0,0 +1,6 @@
+let age = 29;
+const naam = 'Smith';
+const field = 'Computer Science & Engineering';
+
+const info = (userName, userAge, userField) => { return 'Name: ' + userName + ', Age: ' + userAge + ', Field: ' + userField }
+console.log(info(naam, field));
